e6093e20f0e3ebce54801b8fcdc51005f00458d2
Fahad Satti <fahad.satti@gmail.com>
2021-08-11 11:29:54 +0900

    Impelmentation

---
+++ Implementation/results.py
@@ -0,0 +1,231 @@
+import pickle
+import csv
+import numpy
+import io
+import os
+
+
+class Results:
+
+    # BASE_INDEX = 2
+    # FUZZY_WUZZY_INDEX = 1
+    # SYN_AND_SEM_SIM_INDEX = 0
+    computational_iteration = "4.0"
+    amplified_similarity_execution_index = "4.0"
+    models = ['bert-base-nli-stsb-mean-tokens',
+              'bert-large-nli-stsb-mean-tokens',
+              'roberta-base-nli-stsb-mean-tokens',
+              'roberta-large-nli-stsb-mean-tokens',
+              'distilbert-base-nli-stsb-mean-tokens',
+              'bert-base-nli-mean-tokens',
+              'bert-large-nli-mean-tokens',
+              'roberta-base-nli-mean-tokens',
+              'roberta-large-nli-mean-tokens',
+              'distilbert-base-nli-mean-tokens'
+              ]
+    SYN_AND_SEM_SIM_INDEX = 0
+    FUZZY_WUZZY_INDEX = 1
+    BERT_LARGE_NLI_STSB_MEAN_TOKENS_SYN_AND_SEM_INDEX = 2
+    BERT_LARGE_NLI_STSB_MEAN_TOKENS_INDEX = 3
+    BERT_BASE_NLI_MEAN_TOKENS_SYN_AND_SEM_INDEX = 4
+    BERT_BASE_NLI_MEAN_TOKENS_INDEX = 5
+    BERT_BASE_NLI_STSB_MEAN_TOKENS_SYN_AND_SEM_INDEX = 6
+    BERT_BASE_NLI_STSB_MEAN_TOKENS_INDEX = 7
+
+
+    def readData(self, url):
+        data = []
+        with open(url, "rb") as fp:
+            data = pickle.load(fp)
+        return data
+
+    def generateCSV(self, attributes):
+        data = []
+        data.append(
+            ['NodeLeftSchemaName', 'NodeLeftTableName', 'NodeLeftName', 'NodeLeftDataType', 'NodeRightSchemaName',
+             'NodeRightTableName', 'NodeRightName', 'NodeRightDataType', 'BERT_BASE', 'FUZZY_MATCH',
+             'SYN_AND_SEM_MATCH'])
+        for attrIndex, attr in enumerate(attributes):
+            # print("count:", attrIndex)
+            data.append(
+                [attr['nodeLeft']['schemaName'], attr['nodeLeft']['tableName'], attr['nodeLeft']['name'],
+                 attr['nodeLeft']['dataType'], attr['nodeRight']['schemaName'], attr['nodeRight']['tableName'],
+                 attr['nodeRight']['name'], attr['nodeRight']['dataType'], attr['relationshipList'][2]['confidence'],
+                 attr['relationshipList'][1]['confidence'], attr['relationshipList'][0]['confidence']])
+
+        with open(
+                'Data/ComputedResults/resultsv'+self.computational_iteration+'.csv',
+                'w', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerows(data)
+
+    def printBaseHeatMap(self, attributes, modelName, isourMethod= False, syn =0, sem = 0):
+        rowHeaderNodes = []
+        columnHeaderNodes = []
+        matrix = {}
+
+        for attrIndex, attr in enumerate(attributes):
+
+            #print('attr')
+            #print(attr)
+
+
+            leftKey = attr['nodeLeft']['schemaName'] + "_" + attr['nodeLeft']['tableName'] + "_" + attr['nodeLeft'][
+                'name']
+            rightKey = attr['nodeRight']['schemaName'] + "_" + attr['nodeRight']['tableName'] + "_" + attr['nodeRight'][
+                'name']
+            leftKey = leftKey.strip()
+            rightKey = rightKey.strip()
+            # print('leftKey: ', leftKey)
+            # print('rightKey: ', rightKey)
+            #
+            # print('matrix.keys')
+            # print(matrix.keys())
+
+            # print("count:", attrIndex)
+
+            # This ensures the rightKey goes into the column head
+            if rightKey not in columnHeaderNodes:
+                columnHeaderNodes.append(rightKey)
+            # Same for left key
+            if leftKey not in columnHeaderNodes:
+                columnHeaderNodes.append(leftKey)
+
+            if leftKey not in matrix.keys():
+                matrix[leftKey] = {}
+                rowHeaderNodes.append(leftKey)
+            # if (rightKey not in matrix.keys()):
+
+            # Now to look for transposed results
+            if rightKey not in matrix.keys():
+                matrix[rightKey] = {}
+                rowHeaderNodes.append(rightKey)
+
+            synConfidence = 0
+            semConfidence = 0
+            confidence = 0
+
+            for relation in attr['relationshipList']:
+                if isourMethod:
+                    if relation['method'] == modelName+'_SYN_MATCH':
+                        synConfidence = relation['confidence']
+                    elif relation['method'] == modelName + '_SEM_MATCH':
+                        semConfidence = relation['confidence']
+                else:
+                    if relation['method'] == modelName and modelName == 'FUZZY_MATCH':
+                        confidence = float(relation['confidence']/100)
+                    elif(relation['method'] == modelName):
+                        confidence = relation['confidence']
+
+
+
+            if(isourMethod):
+                similarity = (syn * synConfidence) + (sem * semConfidence)
+            else:
+                similarity = confidence
+
+            matrix[leftKey][rightKey] = str(similarity)
+            matrix[rightKey][leftKey] = str(similarity)
+
+
+
+
+            # if (confidenceIndex == 1):
+            #     matrix[leftKey][rightKey] = str(float(attr['relationshipList'][confidenceIndex]['confidence']) / 100)
+            #     matrix[rightKey][leftKey] = str(float(attr['relationshipList'][confidenceIndex]['confidence']) / 100)
+            # else:
+            #     matrix[leftKey][rightKey] = attr['relationshipList'][confidenceIndex]['confidence']
+            #     matrix[rightKey][leftKey] = attr['relationshipList'][confidenceIndex]['confidence']
+
+        #print("Building the " + str(confidenceIndex) + " table")
+
+        rowHeaderNodes.sort()
+        columnHeaderNodes.sort()
+        rowSize = len(rowHeaderNodes)
+        colSize = len(columnHeaderNodes)
+        i, j = 0, 0
+        table = "__,"
+
+        table += ', '.join(columnHeaderNodes)
+        table += '\r\n'
+
+        while (i < rowSize):
+            table += rowHeaderNodes[i] + " ,"
+            j = 0
+            while (j < colSize):
+                # print("rowHeaderNodes[i]=", rowHeaderNodes[i], ", columnHeaderNodes[j]=", columnHeaderNodes[j])
+                # print("matrix[rowHeaderNodes[i]][columnHeaderNodes[j]]=", matrix[rowHeaderNodes[i]][columnHeaderNodes[j]])
+                leftSchema = (rowHeaderNodes[i].split('_')[0]).strip()
+                rightSchema = (columnHeaderNodes[j].split('_')[0]).strip()
+
+                if leftSchema == rightSchema:
+                    table += "-" + ","
+                elif columnHeaderNodes[j] not in matrix[rowHeaderNodes[i]].keys():
+                    print("rowHeaderNodes[i]", rowHeaderNodes[i], "columnHeaderNodes[j]", columnHeaderNodes[j])
+                    table += "-, "
+                else:
+                    # print("leftSchema=", leftSchema, ", rightSchema=", rightSchema, end=';')
+                    table += str(matrix[rowHeaderNodes[i]][columnHeaderNodes[j]]) + ", "
+                j += 1
+            # remove last ,
+            table = table[:-1] + '\r\n'
+            i += 1
+        print()
+        print(modelName+" done. Now saving it:")
+
+        fileName = modelName
+
+        if isourMethod:
+            fileName += '_syn_sem'
+
+        # with open("Data/1.4/" + str(
+        #         fileName) + "-table-V" + "-" + str(syn)+"-"+str(sem)+".csv", "wb") as fp:  # Pickling
+        #     pickle.dump(table, fp)
+        result_dir_path = "Data/"+self.computational_iteration+"/"
+        file_path =  result_dir_path + str(fileName) + "-table-V" + "-" + str(syn)+"-"+str(sem)+".csv"
+        if not os.path.exists(result_dir_path):
+            os.makedirs(result_dir_path)
+
+        with io.open(file_path, 'w', encoding='utf8') as f:
+            f.write(table)
+
+
+        # print(table)
+
+        # print(matrix)
+        # exit()
+
+
+resultObj = Results()
+# data = simObj.readData('Data/dataV03.json')
+data = resultObj.readData('Data/AmplifiedSimilarity-V'+resultObj.amplified_similarity_execution_index+'.txt')
+# data = resultObj.readData('Data/AmplifiedSimilarity-Word2Vec-V'+resultObj.amplified_similarity_execution_index+'.txt')
+
+print('data')
+print(len(data))
+
+
+#resultObj.generateCSV(data)
+
+
+#resultObj.printBaseHeatMap(data,resultObj.SYN_AND_SEM_SIM_INDEX)
+
+#for syn in frange(0.1, 0.9, 0.1):
+
+#resultObj.printBaseHeatMap(data, 'FUZZY_MATCH')
+# resultObj.printBaseHeatMap(data, 'Word2Vec')
+
+
+for model_baseline in resultObj.models:
+    resultObj.printBaseHeatMap(data, model_baseline, True)
+
+for syn in numpy.arange(0, 1, 0.1):
+    syn = round(syn, 1)
+    sem = round((1 - syn), 1)
+    for model_for_syn_sem in resultObj.models:
+        resultObj.printBaseHeatMap(data, model_for_syn_sem, True, syn, sem)
+
+
+
+
+
---
+++ Implementation/similarity.py
@@ -0,0 +1,396 @@
+import ujson as json
+import copy
+import pickle
+from sentence_transformers import SentenceTransformer, util
+from joblib import Parallel, delayed
+import multiprocessing
+import itertools
+import torch
+from numba import njit, cuda, prange
+from numba import jit
+import numpy as np
+#from fastdist import fastdist
+from numpy import dot
+from numpy.linalg import norm
+
+
+# Function to print the settings
+def print_settings():
+    print('Using device:', device)
+    # Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print_settings()
+
+
+@jit(nopython=True, parallel=True)
+def cosine_similarity_numba(u: np.ndarray, v: np.ndarray):
+    assert (u.shape[0] == v.shape[0])
+    uv = 0
+    uu = 0
+    vv = 0
+    for i in prange(u.shape[0]):
+        uv += u[i] * v[i]
+        uu += u[i] * u[i]
+        vv += v[i] * v[i]
+    cos_theta = 1
+    if uu != 0 and vv != 0:
+        cos_theta = uv / np.sqrt(uu * vv)
+    return cos_theta
+
+
+def cosine_similarity_numpy(u: np.ndarray, v: np.ndarray):
+    cos_sim = dot(u, v) / (norm(u) * norm(v))
+    return cos_sim
+
+
+class Similarity:
+
+    def __init__(self):
+        self.embeddingVectors = {}
+        self.models = ['bert-base-nli-stsb-mean-tokens',
+                       'bert-large-nli-stsb-mean-tokens',
+                       'roberta-base-nli-stsb-mean-tokens',
+                       'roberta-large-nli-stsb-mean-tokens',
+                       'distilbert-base-nli-stsb-mean-tokens',
+                       'bert-base-nli-mean-tokens',
+                       'bert-large-nli-mean-tokens',
+                       'roberta-base-nli-mean-tokens',
+                       'roberta-large-nli-mean-tokens',
+                       'distilbert-base-nli-mean-tokens'
+                       ]
+    #     self.models = [
+    # 'roberta-base-nli-mean-tokens',
+    # 'roberta-large-nli-mean-tokens',
+    # 'distilbert-base-nli-mean-tokens'
+    # ]
+        # self.models = ['bert-base-nli-stsb-mean-tokens']
+
+    def readData(self, url):
+
+        with open(url) as json_file:
+            data = json.load(json_file)
+
+        # data = []
+        # with open(url, "rb") as fp:
+        #     data = pickle.load(fp)
+
+        return data
+
+    def getSimpleAttributes(self, attributes):
+
+        print('in getSimpleAttributes function')
+
+        data = []
+        for attr in attributes:
+            data.append(attr['schemaName'] + ' ' + attr['tableName'] + ' ' + attr['name'])
+
+        return data
+
+    def calcualteBaseLineSimilarity(self, attributes, modelName):
+
+        print('len(attributes)')
+        print(len(attributes))
+
+        attributeSimilarities = []
+
+        # model = SentenceTransformer('bert-base-nli-stsb-mean-tokens')
+        model = SentenceTransformer(modelName, device=device)
+
+        for index, attr in enumerate(attributes):
+            if index != 0 and index % 1000 == 0:
+                print("index", index)
+                print_settings()
+            # print(modelName, ' line i: ', i)
+            formatedAttr = copy.deepcopy(attr)
+
+            # embedding1 = model.encode(attr['nodeLeft']['name'], convert_to_tensor=True)
+            embedding1 = model.encode(attr['nodeLeft']['name'])
+            # embedding2 = model.encode(attr['nodeRight']['name'], convert_to_tensor=True)
+            embedding2 = model.encode(attr['nodeRight']['name'])
+
+            # similarity = util.pytorch_cos_sim(embedding1, embedding2)
+            similarity = cosine_similarity_numpy(embedding1, embedding2)
+
+            formatedAttr['relationshipList'].append(
+                {'type': '', 'method': modelName, 'comments': None, 'confidence': "{:.4f}".format(similarity)})
+
+            attributeSimilarities.append(formatedAttr)
+
+        return attributeSimilarities
+
+    def isDataTypeConvertable(self, type1, type2):
+
+        result = False
+
+        if ((type1 == 'Long' and type2 == 'Double') or (type1 == 'Double' and type2 == 'Long')):
+            result = True
+        elif ((type1 == 'Date' and type2 == 'DateTime') or (type1 == 'DateTime' and type2 == 'Date')):
+            result = True
+
+        return result
+
+    # Does not work because it produces very large list of sentences
+    # @staticmethod
+    # def createSentenceForAA(node):
+    #     amplified_sentences = []
+    #     word_concept_map = {}
+    #     # for word in node['suffixArray']:
+    #     #     word_concept_map["word"] = []
+    #     amplified_sentences.append(" ".join(node['suffixArray']))
+    #     for concept in node['conceptArray']:
+    #         if concept['token'] not in word_concept_map:
+    #             word_concept_map[concept['token']] = []
+    #         word_concept_map[concept['token']].append(concept['name'])
+    #     conceptset = word_concept_map.values()
+    #     product_concepts = list(itertools.product(*conceptset))
+    #     for concept_product_tuple in product_concepts:
+    #         amplified_sentences.append(" ".join(concept_product_tuple))
+    #     print(amplified_sentences)
+    #     exit()
+    #     return amplified_sentences
+    # Only suffixes and concepts go into the list and no sentence
+    # @staticmethod
+    # def createSentenceForAA(node):
+    #     amplified_sentences = []
+    #     # word_concept_map = {}
+    #     # for word in node['suffixArray']:
+    #     #     word_concept_map["word"] = []
+    #     amplified_sentences.extend(node['suffixArray'])
+    #     print(amplified_sentences)
+    #     print(node['conceptArray'])
+    #     for concept in node['conceptArray']:
+    #         amplified_sentences.append(concept['name'])
+    #     print(amplified_sentences)
+    #     exit()
+    #     return amplified_sentences
+    # 1 full sentence
+    @staticmethod
+    def createSentenceForAA(node):
+        word_concept_map = {}
+        # for concept in node['conceptArray']:
+        #     if concept['token'] not in word_concept_map:
+        #         word_concept_map[concept['token']] = []
+        #     word_concept_map[concept['token']].append(concept['name'])
+        # # print(word_concept_map)
+        sentence = ""
+        for suffix in node['suffixArray']:
+            sentence += suffix + " " #+ " ".join(word_concept_map[suffix]) + ";"
+
+        return [sentence]
+
+    def getSyntacticSimilarity(self, attr):
+
+        similarity = 0
+
+        if ((attr['nodeLeft']['dataType'] == attr['nodeRight']['dataType']) or (
+                self.isDataTypeConvertable(attr['nodeLeft']['dataType'], attr['nodeRight']['dataType']))):
+            similarity = 1
+
+        return similarity
+
+    def getSemanticSimilarity(self, attrPair, model):
+
+        # formatedattrPair = copy.deepcopy(attrPair)
+        nodeLeftIdentifier = "_".join([attrPair['nodeLeft']['schemaName'], attrPair['nodeLeft']['tableName'],
+                                       attrPair['nodeLeft']['name'], attrPair['nodeLeft']['schemaVersion']])
+        nodeRightIdentifier = "_".join([attrPair['nodeRight']['schemaName'], attrPair['nodeRight']['tableName'],
+                                        attrPair['nodeRight']['name'], attrPair['nodeRight']['schemaVersion']])
+
+        self.embeddingVectors[nodeLeftIdentifier] = Similarity.createSentenceForAA(attrPair['nodeLeft'])
+        self.embeddingVectors[nodeRightIdentifier] = Similarity.createSentenceForAA(attrPair['nodeRight'])
+
+        # print("sentences in left:",len(self.embeddingVectors[nodeLeftIdentifier]))
+        # print(self.embeddingVectors[nodeLeftIdentifier])
+        # print("sentences in right:",len(self.embeddingVectors[nodeRightIdentifier]))
+        # print(self.embeddingVectors[nodeRightIdentifier])
+        # memoization to save time on calculating the embedding vector
+        sentence_encoding_map = {}
+        max_similarity = 0.0
+        avg_similarity = 0.0
+        for term_left in self.embeddingVectors[nodeLeftIdentifier]:
+            sentence_left = ""
+            if isinstance(term_left, list):
+                sentence_left = " ".join(term_left)
+            else:
+                sentence_left = term_left
+
+            if sentence_left not in sentence_encoding_map:
+                # embedding1 = model.encode(term_left, convert_to_tensor=True)
+                embedding1 = model.encode(sentence_left)
+                sentence_encoding_map[sentence_left] = embedding1
+            else:
+                embedding1 = sentence_encoding_map[sentence_left]
+            # this similarity is between 0 and 1
+            for term_right in self.embeddingVectors[nodeRightIdentifier]:
+                sentence_right = ""
+                if isinstance(term_right, list):
+                    sentence_right = " ".join(term_right)
+                else:
+                    sentence_right = term_right
+
+                if sentence_left == sentence_right:
+                    # The two sentences are equal
+                    return float(1.0)
+
+                if sentence_right not in sentence_encoding_map:
+                    # embedding2 = model.encode(term_right, convert_to_tensor=True)
+                    embedding2 = model.encode(sentence_right)
+                    sentence_encoding_map[sentence_right] = embedding2
+                else:
+                    embedding2 = sentence_encoding_map[sentence_right]
+
+                # term_similarity = util.pytorch_cos_sim(embedding1, embedding2)
+                # term_similarity1 = cosine_similarity_numba(embedding1, embedding2)
+                term_similarity = dot(embedding1, embedding2) / (norm(embedding1) * norm(embedding2))
+                # term_similarity = cosine_similarity_numpy(embedding1, embedding2)
+
+                # if float(term_similarity[0][0]) == float(1.0):
+
+                if float(term_similarity) == float(1.0):
+                    # The two embeddings are equal but not the sentence, highly unlikely!
+                    print("Same embeddings")
+                    print(sentence_left)
+                    print(sentence_right)
+                    return float(1.0)
+
+                if max_similarity == float(0.0) and term_similarity != float(0.0):
+                    max_similarity = term_similarity
+
+                if float(term_similarity) > max_similarity:
+                    max_similarity = float(term_similarity)
+
+        # calculate the average similarity of the disjoint union of two sets
+        # final_similarity = max_left_term_similarity / len(self.embeddingVectors[nodeLeftIdentifier])
+        final_similarity = max_similarity
+        # print("avg_similarity:",avg_similarity)
+        # nodeLeftContext = self.embeddingVectors[nodeLeftIdentifier]
+        # nodeRightContext = self.embeddingVectors[nodeRightIdentifier]
+
+        # embedding1 = model.encode(nodeLeftContext, convert_to_tensor=True)
+        # embedding2 = model.encode(nodeRightContext, convert_to_tensor=True)
+        #
+        # similarity = util.pytorch_cos_sim(embedding1, embedding2)
+        # similarity = "{:.4f}".format(similarity[0][0])
+        # if final_similarity > float(0.0):
+        #     print("final_similarity:", final_similarity)
+        return final_similarity
+
+    def calculateAmplifiedSimilarity(self, attributes, modelName):
+
+        attributeSimilarities = []
+
+        print('len(attributes)')
+        print(len(attributes))
+
+        model = SentenceTransformer(modelName, device=device)
+
+        for index, attrPair in enumerate(attributes):
+            if index != 0 and index % 1000 == 0:
+                print("index", index)
+                print_settings()
+            formatedAttr = copy.deepcopy(attrPair)
+
+            syntacticSimilarity = self.getSyntacticSimilarity(attrPair)
+            semanticSimilarity = self.getSemanticSimilarity(attrPair, model)
+            # similarity = self.getSemanticSimilarity(attrPair, model)
+
+            # print("syntacticSimilarity: ", syntacticSimilarity, ', semanticSimilarity: ', semanticSimilarity)
+            # print('Semantic index: ', index)
+
+            # similarity = (0.5 * float(syntacticSimilarity)) + (0.5 * float(semanticSimilarity))
+
+            formatedAttr['relationshipList'].append(
+                {'type': '', 'method': modelName + '_SYN_MATCH', 'comments': None, 'confidence': syntacticSimilarity})
+            formatedAttr['relationshipList'].append(
+                {'type': '', 'method': modelName + '_SEM_MATCH', 'comments': None, 'confidence': semanticSimilarity})
+            attributeSimilarities.append(formatedAttr)
+
+        return attributeSimilarities
+
+    # function optimized to run on gpu
+    # @jit(target="cuda")
+
+    # def processSchemaMapNode(self, attrPair, model):
+    #     # formatedattrPair = copy.deepcopy(attrPair)
+    #     nodeLeftIdentifier = "_".join([attrPair['nodeLeft']['schemaName'], attrPair['nodeLeft']['tableName'],
+    #                                    attrPair['nodeLeft']['name'], attrPair['nodeLeft']['schemaVersion']])
+    #     nodeRightIdentifier = "_".join([attrPair['nodeRight']['schemaName'], attrPair['nodeRight']['tableName'],
+    #                                     attrPair['nodeRight']['name'], attrPair['nodeRight']['schemaVersion']])
+    #
+    #     self.embeddingVectors[nodeLeftIdentifier] = self.createSentenceForAA(attrPair['nodeLeft'])
+    #     self.embeddingVectors[nodeRightIdentifier] = self.createSentenceForAA(attrPair['nodeRight'])
+    #
+    #     # if nodeLeftIdentifier not in self.embeddingVectors:
+    #     #     self.embeddingVectors[nodeLeftIdentifier] = self.createSentenceForAA(attrPair['nodeLeft'])
+    #     #
+    #     # if nodeRightIdentifier not in self.embeddingVectors:
+    #     #     self.embeddingVectors[nodeRightIdentifier] = self.createSentenceForAA(attrPair['nodeRight'])
+    #     max_similarity = 0.0
+    #     max_sentence_left = ""
+    #     max_sentence_right = ""
+    #     for sentence_left in self.embeddingVectors[nodeLeftIdentifier]:
+    #         for sentence_right in self.embeddingVectors[nodeRightIdentifier]:
+    #             embedding1 = model.encode(sentence_left, convert_to_tensor=True)
+    #             embedding2 = model.encode(sentence_right, convert_to_tensor=True)
+    #             similarity = util.pytorch_cos_sim(embedding1, embedding2)
+    #             similarity = "{:.4f}".format(similarity[0][0])
+    #             if float(similarity) > max_similarity:
+    #                 max_similarity = float(similarity)
+    #                 max_sentence_left = sentence_left
+    #                 max_sentence_right = max_sentence_right
+    #
+    #     i = 0
+    #     while i < len(attrPair['relationshipList']):
+    #         if attrPair['relationshipList'][i]['method'] == 'SYN_AND_SEM_MATCH':
+    #             attrPair['relationshipList'][i]['sentence_left'] = max_sentence_left
+    #             attrPair['relationshipList'][i]['sentence_right'] = max_sentence_right
+    #             attrPair['relationshipList'][i]['confidence'] = max_similarity
+    #             break
+    #
+    #     return attrPair
+
+    def calculateSimilarity(self, attributes):
+
+        for model in self.models:
+            print('model: ', model)
+            attributes = self.calculateAmplifiedSimilarity(attributes, model)
+
+            # attributes = self.calcualteBaseLineSimilarity(attributes, model)
+
+        with open("Data/AmplifiedSimilarity-V4.0.txt", "wb") as fp:  # Pickling
+            pickle.dump(attributes, fp)
+
+        return attributes
+
+
+num_cores = multiprocessing.cpu_count() - 1
+print("num_cores:", num_cores)
+
+simObj = Similarity()
+data = simObj.readData('Data/schema_processed_1617174847206.json')
+
+print('data')
+print(len(data))
+
+calculatedSim = simObj.calculateSimilarity(data);
+
+#data = simObj.calcualteBaseLineSimilarity(data)
+
+
+#similarity = simObj.calculateSimilarity(data)
+
+# synAndSemSimilarity = simObj.calculateAmplifiedSimilarity(data)
+
+# print('synAndSemSimilarity')
+# print(synAndSemSimilarity)
+
+# print('synAndSemSimilarity')
+# print(synAndSemSimilarity)
+
